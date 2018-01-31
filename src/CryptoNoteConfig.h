c7f43faad9fb2ed13125283e3afcda9ff18ff04d
Adam <adam@ETN2.PILBEAMS>
2018-01-31 08:04:58 -0800

    Further updates for initial release.

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,9 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"your_seed_ip1.com:8080",
-  //"your_seed_ip2.com:8080",
+  "208.115.104.160:19757",
+  "208.115.104.159:19757",
+  "208.115.99.25:19757",
 };
 
 struct CheckpointData {
